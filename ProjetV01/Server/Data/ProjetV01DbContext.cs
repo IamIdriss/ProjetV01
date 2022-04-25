9be29034364dfa04fb8afd5925a965bac649d755
Driss <idrissisamm@gmail.com>
2022-04-25 20:18:02 +0100

    Create DbContext Server side

--- ProjetV01/Server/Data/ProjetV01DbContext.cs
+++ ProjetV01/Server/Data/ProjetV01DbContext.cs
@@ -15,6 +15,28 @@ namespace ProjetV01.Server.Data
         {
         }
 
+        private const string connectionString = "Server=DESKTOP-JDGBI4U;Database=ProjetV01DB;Trusted_Connection=True;MultipleActiveResultSets=true;";
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ProjetV01.Shared.Mail>()
+                .Property(Mail => Mail.Id).ValueGeneratedOnAdd();
+
+            modelBuilder.Entity<ProjetV01.Shared.Agent>()
+               .Property(Agent => Agent.Id).ValueGeneratedOnAdd();
+
+            modelBuilder.Entity<ProjetV01.Shared.Position>()
+               .Property(Position => Position.Id).ValueGeneratedOnAdd();
+
+        }
+
         public DbSet<ProjetV01.Shared.Agent> Agent { get; set; }
 
         public DbSet<ProjetV01.Shared.Mail> Mail { get; set; }
